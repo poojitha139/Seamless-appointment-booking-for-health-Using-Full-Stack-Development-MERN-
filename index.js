5c6fef11dc997479c1f61eb32791d0e4aefa28a1
poojitha139 <152069941+poojitha139@users.noreply.github.com>
2025-07-17 15:52:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectToDB = require("./config/connectToDB");
-
-const app = express();
-//////dotenv config/////////////////////
-dotenv.config();
-connectToDB();
-const PORT = process.env.PORT;
-
-/////////////////middlewares////////////////
-app.use(express.json());
-app.use(cors());
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ message: "Something went wrong", success: false });
-});
-
-/////////routes//////////////////////
-app.use('/api/user/', require('./routes/userRoutes'))
-app.use('/api/admin/', require('./routes/adminRoutes'))
-app.use('/api/doctor',require('./routes/doctorRoutes'))
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
